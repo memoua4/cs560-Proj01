65e133ce2ddcab969817269315b0991cf50b6d2b
Mai Moua <memoua@email.arizona.edu>
2017-02-23 13:36:28 -0600

    HeapFile.c

--- HeapFile/src/heapfile.C
+++ HeapFile/src/heapfile.C
@@ -62,7 +62,8 @@ HeapFile::HeapFile( const char *name, Status& returnStatus )
 HeapFile::~HeapFile()
 {
    // fill in the body
-
+    deleted_file = 1;
+    delete [] fileName;
 }
 
 // *************************************
@@ -75,10 +76,55 @@ int HeapFile::getRecCnt()
 
 // *****************************
 // Insert a record into the file
+/*
+ * Function Name: insertRecord
+ * @params: 
+ *              recPtr = the record to be stored into data[] 
+ *              recLen = the size of the record
+ *              outRid = passed by reference to store the record being stored 
+ *  Description: Initially, we want to pin the first page and page id into the buffer
+ *  manager (request page in frame). It will then attempt to insert the record. If 
+ *  it insert correctly, it will return OK as the status. There are multiple reasons why
+ *  it didn't insert properly:
+ *          1) The current page is full, it will then have to check for the next page
+ *          2) There are no pages that have available space, therefore, it must create
+ *          a new page and insert the record as the first record
+ *  If it cannot insert, it should return DONE or FAIL?
+ */
 Status HeapFile::insertRecord(char *recPtr, int recLen, RID& outRid)
 {
     // fill in the body
-    return OK;
+    PageId curPageId = firstDirPageId; 
+    PageId prevPageId = INVALID_PAGE;
+    HFPage curPage;
+    Status status;
+
+    while (true) {
+        // pin page
+        MINIBASE_DB->pinPage(curPageId, (Page *)&curPage);
+        status = curPage->insertRecord(recPtr, recLen, outRid);
+        if ( status != OK ) {
+            // get next page id
+            prevPageId = curPageId;
+            curPageId = curPage->getNextPage();
+        }
+        // unpin page
+        MINIBASE_DB->unpinPage(curPageId);
+    
+        if ( status == OK ) 
+            break;
+
+        if ( curPageId == INVALID_PAGE ) {
+            HFPage newPage;
+            MINIBASE_DB->pinPage(prevPageId, (Page *)&curPage);
+            MINIBASE_DB->newPage(curPageId, (Page *)&newPage);
+            curPage->setNextPage(newPage);
+            status = newPage->insertRecord(recPtr, recLen, outRid);
+            MINIBASE_DB->unpinPage(prevPageId);
+            break;
+        }
+    }
+    return status;
 } 
 
 
@@ -95,7 +141,35 @@ Status HeapFile::deleteRecord (const RID& rid)
 Status HeapFile::updateRecord (const RID& rid, char *recPtr, int recLen)
 {
   // fill in the body
-  return OK;
+    PageId curPageId = firstDirPageId;
+    PageId prevPageId;
+    HFPage curPage;
+    Status status;
+    char *updateRec;
+    int updateLen;
+
+    while (curPageId != NULL) {
+        MINIBASE_BM->pinPage(curPageId, (Page *&)curPage);
+        //HFPage::getRecord(RID rid, char* recPtr, int& recLen)
+        status = curPage->getRecord(rid, &updateRecord, &updateLen);
+        if (status != OK) {
+            prevPageId = curPageId;
+            curPageId = curPage->getNextPage();
+        }
+        MINIBASE_BM->unpinPage(prevPageId);
+
+        if ( status == OK ) 
+            break;
+    }
+    if ( curPageId == INVALID_PAGE ) 
+        return DONE; // file does not exists 
+
+    if (updateLen != recLen)
+        return FAIL; // file could not update
+
+    memcpy(recPtr, updateRecord, recLen);
+    return OK;
+
 }
 
 // ***************************************************
@@ -111,7 +185,8 @@ Status HeapFile::getRecord (const RID& rid, char *recPtr, int& recLen)
 Scan *HeapFile::openScan(Status& status)
 {
   // fill in the body 
-  return NULL;
+    status = Scan::Scan(this, status);
+    return NULL;
 }
 
 // ****************************************************
@@ -127,8 +202,17 @@ Status HeapFile::deleteFile()
 // (Allocate pages in the db file via buffer manager)
 Status HeapFile::newDataPage(DataPageInfo *dpinfop)
 {
-
     // fill in the body
+    PageId newPageId;
+    Page *newPage;
+    MINIBASE_BM->newPage(newPageId, newPage); // create a new page
+
+    HFPage hfpage;
+    hfpage->init(newPageId);
+
+    dpinfop.availableSapce = hfpage->available_space();
+    dpinfop.recct = 0;
+    dpinfop.pageId = hfpage->page_no();
     return OK;
 }
 
