e8b07663610b5f7e9311123af4bc9657bea554d7
DavidM1A2 <davidslovikosky@email.arizona.edu>
2017-02-26 18:01:10 -0700

    Merged scan

--- HeapFile/src/scan.C
+++ HeapFile/src/scan.C
@@ -32,10 +32,32 @@ Scan::~Scan()
 // *******************************************
 // Retrieve the next record in a sequential scan.
 // Also returns the RID of the retrieved record.
-Status Scan::getNext(RID& rid, char *recPtr, int& recLen)
+Status Scan::getNext(RID &rid, char *recPtr, int &recLen)
 {
-  // put your code here
-  return OK;
+    Status status;
+
+    // Check if we have a valid datapage to grab from
+    if (nxtUserStatus != OK)
+    {
+        status = nextDataPage();
+        if (status != OK)
+            return MINIBASE_CHAIN_ERROR(SCAN, status);
+    }
+
+    // Grab all the other data we need to return
+    // This will fill in recPtr and recLen
+    status = dataPage->getRecord(userRid, recPtr, recLen);
+    if (status != OK)
+        return MINIBASE_CHAIN_ERROR(SCAN, status);
+
+    // Fill in rid
+    rid = userRid;
+
+    // Move userRid to the next location, and put that result into the nxtUserStatus
+    // variable indicating if we have another record
+    nxtUserStatus = dataPage->nextRecord(rid, userRid);
+
+    return status;
 }
 
 // *******************************************
@@ -52,8 +74,24 @@ Status Scan::init(HeapFile *hf)
 // Reset everything and unpin all pages.
 Status Scan::reset()
 {
-  // put your code here
-  return OK;
+    Status status;
+
+    if (dataPage != NULL)
+    {
+        // Unpin the dataPage
+        MINIBASE_BM->unpinPage(dataPageId);
+        dataPage = NULL;
+        dataPageId = 0;
+    }
+    if (dirPage != NULL)
+    {
+        // Unpin the dirPage
+        MINIBASE_BM->unpinPage(dirPageId);
+        dirPage = NULL;
+        dirPageId = 0;
+    }
+    scanIsDone = false;
+    nxtUserStatus = OK;
 }
 
 // *******************************************
@@ -88,9 +126,71 @@ Status Scan::firstDataPage()
 
 // *******************************************
 // Retrieve the next data page.
-Status Scan::nextDataPage(){
-  // put your code here
-  return OK;
+Status Scan::nextDataPage()
+{
+    Status status;
+
+    // dataPage might be null if this is the first call to nextDataPage
+    if (dataPage == NULL)
+    {
+        // Make sure that this current page ID is not invalid
+        if (dataPageId == INVALID_PAGE) return DONE;
+
+        // Pin the page
+        MINIBASE_BM->pinPage(dataPageId, (Page *&) dataPage);
+        // Grab the first record and place RID into the private data field
+        Status gotFirst = dataPage->firstRecord(userRid);
+        // If we got a first record, just return it
+        if (gotFirst == OK) return OK;
+        else                return MINIBASE_CHAIN_ERROR(SCAN, gotFirst);
+    }
+
+    // Unpin the current data page
+    MINIBASE_BM->unpinPage(dataPageId);
+
+    // Retrieve the next DataPageInfo
+    RID nextDataPageInfoRID;
+    // Retrieve the next dataPage from the directory
+    status = dirPage->nextRecord(dataPageRid, nextDataPageInfoRID);
+    // Done means it's time to move onto the next directory page
+    if (status == DONE)
+    {
+        Status hasNextDirPage = nextDirPage();
+        if (hasNextDirPage == OK)
+        {
+            // Retrieve the next dataPage from the directory
+            status = dirPage->firstRecord(nextDataPageInfoRID);
+        }
+            // if no next directory pages exists, we're done with the scan
+        else if (hasNextDirPage == DONE)
+        {
+            scanIsDone = true;
+            return DONE;
+        }
+        else return MINIBASE_CHAIN_ERROR(SCAN, status);
+    }
+        // Move on ONLY if status == ok
+    else if (status != OK) return MINIBASE_CHAIN_ERROR(SCAN, status);
+
+    // We got the next dataPageRid to read from
+    dataPageRid = nextDataPageInfoRID;
+
+    // Grab the DataPageInfo from the directory page
+    DataPageInfo *info;
+    int length;
+    dirPage->getRecord(dataPageRid, (char *) info, length);
+
+    // Set the new dataPageId
+    dataPageId = info->pageId;
+    // Set the new dataPage
+    MINIBASE_BM->pinPage(dataPageId, (Page *&) dataPage);
+    // Set the userRid
+    dataPage->firstRecord(userRid);
+    // Set nxtUserStatus
+    RID temp;
+    nxtUserStatus = dataPage->nextRecord(userRid, temp);
+
+    return OK;
 }
 
 // *******************************************
