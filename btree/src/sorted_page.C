bae644f3a6fe4f1236e2b9d648df0a3863ff75a4
DavidM1A2 <davidslovikosky@email.arizona.edu>
2017-04-11 21:35:11 -0700

    Fixed the sorting

--- btree/src/sorted_page.C
+++ btree/src/sorted_page.C
@@ -48,7 +48,7 @@ Status SortedPage::insertRecord(AttrType keyType,
         return MINIBASE_FIRST_ERROR(SORTEDPAGE, ATTRNOTFOUND);
 
     // Now sort the data using a lambda function
-    std::sort(slot, slot + slotCnt, [&](const slot_t first, const slot_t second) -> bool {
+    std::sort(slot, slot + this->slotCnt + 1, [&](const slot_t first, const slot_t second) -> bool {
         if (first.length == EMPTY_SLOT)
             return false;
         if (second.length == EMPTY_SLOT)
